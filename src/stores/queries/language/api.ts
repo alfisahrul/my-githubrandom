2f26ff5d65210b5b4e1cb8b68bed538e243ec690
Alfisahrul <72127376+alfisahrul@users.noreply.github.com>
2024-11-04 15:10:39 +0700

    fix: language api structure

--- src/stores/queries/language/api.ts
+++ src/stores/queries/language/api.ts
@@ -2,9 +2,11 @@ import { Language } from "../../../interfaces/entities/language";
 import { CommonResponse } from "../../../utils/common";
 import { apiGet } from "../../../utils/api";
 
-export const getLanguage = async (): Promise<CommonResponse<Language>> => {
+export const getLanguage = async (): Promise<CommonResponse<Language[]>> => {
     const url = "https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json"
-    const response = await apiGet<CommonResponse<Language>>(url)
-    return response.data
-    throw Error("Failed to fetch Language");
+    const response = await apiGet<CommonResponse<Language[]>>(url)
+    if (!response || !response.data) {
+        throw new Error("Failed to fetch languages.");
+    }
+    return response.data; 
 }
--- src/stores/queries/language/queries.ts
+++ src/stores/queries/language/queries.ts
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLanguage } from "./api";
+import { Language } from "../../../interfaces/entities/language";
+import { CommonResponse } from "../../../utils/common";
 
 export const useLanguage = () => {
-    return useQuery({
+    return useQuery<CommonResponse<Language[]>>({
         queryKey: ["language"],
         queryFn: () => getLanguage(),
     })
