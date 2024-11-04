d53f53bff4f63b9f3b55386c4dca75859040c756
Alfisahrul <72127376+alfisahrul@users.noreply.github.com>
2024-11-04 15:09:41 +0700

    feat: repositories api data

---
+++ src/stores/queries/repositories/api.ts
@@ -0,0 +1,16 @@
+// fetching data from url
+
+import { GithubRepositories } from "../../../interfaces/entities/repositories";
+import { apiGet } from "../../../utils/api";
+import { CommonResponse } from "../../../utils/common";
+
+export const getRepositories = async () : Promise<CommonResponse<GithubRepositories[]>> =>{
+    const url  = "https://api.github.com/search/repositories?q=language:csharp"
+
+    const response = await apiGet<CommonResponse<GithubRepositories>>(url)
+    if (!response || response.data){
+        throw new Error("Failed to fetch Repositories");
+
+    }
+    return response.data;
+}
---
+++ src/stores/queries/repositories/queries.ts
@@ -0,0 +1,13 @@
+// get data from fetching model
+
+import { useQuery } from "@tanstack/react-query"
+import { getRepositories } from "./api"
+import { CommonResponse } from "../../../utils/common"
+import { GithubRepositories } from "../../../interfaces/entities/repositories"
+
+export const useGithubRepositories = () => {
+    return useQuery<CommonResponse<GithubRepositories[]>>({
+        queryKey:["githubRepositories"],
+        queryFn : () => getRepositories(),
+    })
+}
