9e5c70427ad130d44a0faf8cb6829837dbcecb03
Alfisahrul <72127376+alfisahrul@users.noreply.github.com>
2024-11-06 11:15:19 +0700

    feat: get random url

--- src/stores/queries/repositories/api.ts
+++ src/stores/queries/repositories/api.ts
@@ -2,25 +2,30 @@ import { GithubRepositories } from "../../../interfaces/entities/repositories";
 import { apiGet } from "../../../utils/api";
 import { CommonResponse } from "../../../utils/common";
 
-export const getRepositories = async (): Promise<CommonResponse<GithubRepositories>> => {
-    const url = `https://api.github.com/search/repositories?q=language:csharp&per_page=1&page=3`;
+export const getRepositories = async (language: string): Promise<CommonResponse<GithubRepositories>> => {
+    const validLanguage = language || "javascript"
+    const randomPage = Math.floor(Math.random() * 10) + 1;
+
+    const url = `https://api.github.com/search/repositories?q=language:${encodeURI(validLanguage)}&page=${randomPage}`;
+    
     const response = await apiGet<GithubRepositories>(url);
 
     if (!response || !response.data) {
-        throw new Error("Failed to fetch Repositories");
+        throw new Error("Failed to fetch repositories");
     }
-    function convertKeysToCamelCase(obj: any): any {
+
+    function convertKeysToCamelCase<T>(obj: any): T {
         if (Array.isArray(obj)) {
-            return obj.map(convertKeysToCamelCase);
+            return obj.map(convertKeysToCamelCase) as unknown as T;
         } else if (obj !== null && obj.constructor === Object) {
             return Object.entries(obj).reduce((acc, [key, value]) => {
                 const camelKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
                 acc[camelKey] = convertKeysToCamelCase(value);
                 return acc;
-            }, {} as Record<string, any>);
+            }, {} as Record<string, any>) as T;
         }
         return obj;
     }
 
-    return convertKeysToCamelCase(response);
-}
+    return convertKeysToCamelCase<CommonResponse<GithubRepositories>>(response);
+};
